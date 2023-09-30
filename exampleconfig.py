ec7e01754e4792148d2772450ea7155c577c0983
Team Neiman <136953008+NEIMAN-AI@users.noreply.github.com>
2023-09-30 13:29:55 +0530

    Create exampleconfig.py

---
+++ exampleconfig.py
@@ -0,0 +1,27 @@
+from sample_config import Config
+
+
+class Development(Config):
+    # get this values from the my.telegram.org
+    APP_ID = 26969696
+
+    API_HASH = ""
+    # the name to display in your alive message
+    ALIVE_NAME = "Neiman"
+    # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
+    DB_URI = ""
+    # After cloning the repo and installing requirements do python3 telesetup.py an fill th this
+    LEGEND_STRING = ""
+    # create a new bot in @botfather and fill the following vales with bottoken
+    BOT_TOKEN = ""
+    # command handler
+    HANDLER = "."
+    # command hanler for sudo
+    SUDO_HANDLER = "."
+    # External plugins repo
+    EXTERNAL_REPO = True
+    EXTERNAL_REPOBRANCH = "main"
+    UPSTREAM_REPO = "pro"
+    VCMODE = False
+    # Your City's TimeZone
+    TZ = "Asia/Kolkata"
